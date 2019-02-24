e12cf93b9991146f0c01c5bdf084a5c71799338b
psywalker <psywalker09@gmail.com>
2019-02-24 20:44:54 +0300

    Updates

---
+++ precache-manifest.8cd4dba9fad98be5dd6c556201ddfa0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "458a3c034b9119861542",
+    "url": "/Trello-Demo/static/css/main.e0580ae0.chunk.css"
+  },
+  {
+    "revision": "458a3c034b9119861542",
+    "url": "/Trello-Demo/static/js/main.458a3c03.chunk.js"
+  },
+  {
+    "revision": "bb3bb5a968f0fe74fc96",
+    "url": "/Trello-Demo/static/js/runtime~main.bb3bb5a9.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/Trello-Demo/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Trello-Demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2db2b978eec200f7218f7c8408f852cc",
+    "url": "/Trello-Demo/index.html"
+  }
+];
