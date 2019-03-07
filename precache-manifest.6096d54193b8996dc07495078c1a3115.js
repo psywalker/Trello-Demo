d366dd863dffd01635572f44c9428dc0c78ed8df
psywalker <psywalker09@gmail.com>
2019-03-07 21:51:09 +0300

    Updates

--- precache-manifest.6096d54193b8996dc07495078c1a3115.js
+++ precache-manifest.6096d54193b8996dc07495078c1a3115.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "eeb4b7176d93960b88ad",
-    "url": "/Trello-Demo/static/css/main.9d5a769d.chunk.css"
+    "revision": "10723b4d3dc97d308e62",
+    "url": "/Trello-Demo/static/css/main.15d90230.chunk.css"
   },
   {
-    "revision": "eeb4b7176d93960b88ad",
-    "url": "/Trello-Demo/static/js/main.eeb4b717.chunk.js"
+    "revision": "10723b4d3dc97d308e62",
+    "url": "/Trello-Demo/static/js/main.10723b4d.chunk.js"
   },
   {
     "revision": "bb3bb5a968f0fe74fc96",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/Trello-Demo/static/js/2.62daa1da.chunk.js"
   },
   {
-    "revision": "c9b4f0abb50a5450e5ce365dfe3326ce",
+    "revision": "5e06831e8f40357dd4c110cc07cde323",
     "url": "/Trello-Demo/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Trello-Demo/precache-manifest.b8bc663d9e1cfb245880f9e50a70b014.js"
+  "/Trello-Demo/precache-manifest.6096d54193b8996dc07495078c1a3115.js"
 );
 
 workbox.clientsClaim();
